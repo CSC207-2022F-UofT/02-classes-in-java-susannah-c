85f5daedcf5b614966ea6c4e29010b2ce85a4b76
susannah-c <susannah.cao@mail.utoronto.ca>
2022-10-09 17:37:22 -0400

    Exercise 02 Submission

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -87,7 +87,7 @@ public class Silly implements Comparable<Silly>{
      * @param lastName a string for the last name of this Silly instance
      * finish documentation
      */
-    public Silly(String firstName, String lastName) {
+    public Silly(String firstName, String lastName)  {
         this.name = firstName + lastName;
     }
 
@@ -124,7 +124,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {1, 2, 3, 4};
+        int[] expected_values = {0, 1, 2, 3};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
